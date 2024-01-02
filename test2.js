dce4e353b9686cb4a57203ba28049cb4d9dca9cd
CharC0de <eganocharlesreinerg@gmail.com>
2024-01-02 20:38:21 +0800

    Add initial GUI

---
+++ test2.js
@@ -0,0 +1,51 @@
+let key = "apple";
+const alpha = "abcdefghijklmnopqrstuvwxyz";
+
+let input = "University of Science and Technology";
+
+let i = 0;
+let decrypt = false;
+
+keyString = input
+  .split("")
+  .map((char) => {
+    if (/\w/i.test(char)) {
+      i = i === key.length ? 0 : i;
+      let keyVal = key[i];
+      ++i;
+      return keyVal;
+    }
+    return char;
+  })
+  .join("");
+
+console.log(
+  input
+    .split("")
+    .map((char, i) =>
+      /\w/i.test(char) && !decrypt
+        ? alpha.split("")[
+            alpha.indexOf(char.toLowerCase()) +
+              alpha.indexOf(keyString.split("")[i].toLowerCase()) >
+            alpha.length
+              ? alpha.indexOf(char.toLowerCase()) +
+                alpha.indexOf(keyString.split("")[i].toLowerCase()) -
+                alpha.length
+              : alpha.indexOf(char.toLowerCase()) +
+                alpha.indexOf(keyString.split("")[i].toLowerCase())
+          ]
+        : /\w/i.test(char) && decrypt
+        ? alpha.split("")[
+            alpha.indexOf(char.toLowerCase()) -
+              alpha.indexOf(keyString.split("")[i].toLowerCase()) <
+            0
+              ? alpha.indexOf(char.toLowerCase()) -
+                alpha.indexOf(keyString.split("")[i].toLowerCase()) +
+                alpha.length
+              : alpha.indexOf(char.toLowerCase()) -
+                alpha.indexOf(keyString.split("")[i].toLowerCase())
+          ]
+        : char
+    )
+    .join("")
+);
